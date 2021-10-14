18d18ecbf0e9230e44330834e9a94053bffd6437
tyranron <tyranron@gmail.com>
2021-10-14 17:42:29 +0300

    Some corrections [skip ci]

--- core/src/es/adapter/transformer/strategy/into.rs
+++ core/src/es/adapter/transformer/strategy/into.rs
@@ -28,7 +28,7 @@ where
     type Transformed = IntoEvent;
     type TransformedStream<'out> = stream::MapOk<
         InnerStrategy::TransformedStream<'out>,
-        IntoFn<InnerStrategy::Transformed, IntoEvent>,
+        fn(InnerStrategy::Transformed) -> IntoEvent,
     >;
 
     fn transform<'me: 'out, 'ctx: 'out, 'out>(
@@ -39,5 +39,3 @@ where
         InnerStrategy::transform(adapter, event, ctx).map_ok(IntoEvent::from)
     }
 }
-
-type IntoFn<FromEvent, IntoEvent> = fn(FromEvent) -> IntoEvent;
